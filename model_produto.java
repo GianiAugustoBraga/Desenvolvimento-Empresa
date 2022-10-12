def48ec6c940c0846bb96ed91fb6ea2531bfa9bc
GianiAugustoBraga <115471128+GianiAugustoBraga@users.noreply.github.com>
2022-10-12 17:19:37 -0300

    Add files via upload

---
+++ model_produto.java
@@ -0,0 +1,57 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Aluno
+ */
+public class model_produto {
+    
+    private int codigo;
+    private String nome;
+    private int quant;
+    private double valor;
+
+    
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    
+    
+    
+}
